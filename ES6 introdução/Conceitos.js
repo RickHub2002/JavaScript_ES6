90f14758955a6dbd8931cc812f46d40d056d1452
unknown <targinoricardo123@gmail.com>
2021-10-07 13:15:56 -0300

    Inclusão da pasta: introdução ao javaScript ES6

---
+++ ES6 introdução/Conceitos.js
@@ -0,0 +1,9 @@
+var meuNumero = 20;
+var meuTexto = 'Exemplo';
+
+console.log(meuNumero + meuTexto);
+//Fracamente Tipada
+
+var meuNumero2 = 20; //Int
+var meuTexto2 = 'Exemplo'; //String
+//Tipagem dinâmica
