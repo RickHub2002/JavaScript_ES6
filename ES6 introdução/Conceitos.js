c1a15defc5b75f0f31a374a56750a613c3b67b7a
Ricardo Targino <64715326+RickHub2002@users.noreply.github.com>
2021-10-22 19:57:08 -0300

    Delete ES6 introdução directory

--- ES6 introdução/Conceitos.js
+++
@@ -1,9 +0,0 @@
-var meuNumero = 20;
-var meuTexto = 'Exemplo';
-
-console.log(meuNumero + meuTexto);
-//Fracamente Tipada
-
-var meuNumero2 = 20; //Int
-var meuTexto2 = 'Exemplo'; //String
-//Tipagem dinâmica
